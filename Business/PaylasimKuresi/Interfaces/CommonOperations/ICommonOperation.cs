73f1872f1759981718450dd675817370679557c5
o_gkmn <ozgur.gokmen735@gmail.com>
2024-05-04 17:06:38 +0300

    Changed return type of CommonOperations' CreateAsync and UpdateAsync methods to TGet

--- Business/PaylasimKuresi/Interfaces/CommonOperations/ICommonOperation.cs
+++ Business/PaylasimKuresi/Interfaces/CommonOperations/ICommonOperation.cs
@@ -6,7 +6,7 @@ public interface ICommonOperation<TCreate, TUpdate, TGet>
 {
     Task<TGet?> GetAsync(Expression<Func<TGet, bool>> filter);
     Task<List<TGet>> GetListAsync(Expression<Func<TGet, bool>>? filter = null);
-    Task<TCreate> CreateAsync(TCreate entityDto);
-    Task<TUpdate> UpdateAsync(TUpdate entityDto);
+    Task<TGet> CreateAsync(TCreate entityDto);
+    Task<TGet> UpdateAsync(TUpdate entityDto);
     Task<bool> DeleteAsync(TGet entityDto);
 }
