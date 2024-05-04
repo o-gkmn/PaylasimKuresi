e2a4556112534f9918fd49118e4181d66d3ad4e9
o_gkmn <ozgur.gokmen735@gmail.com>
2024-05-04 14:20:47 +0300

    Renamed CommonOperations method parameters

--- Business/PaylasimKuresi/Interfaces/CommonOperations/ICommonOperation.cs
+++ Business/PaylasimKuresi/Interfaces/CommonOperations/ICommonOperation.cs
@@ -6,7 +6,7 @@ public interface ICommonOperation<TCreate, TUpdate, TGet>
 {
     Task<TGet?> GetAsync(Expression<Func<TGet, bool>> filter);
     Task<List<TGet>> GetListAsync(Expression<Func<TGet, bool>>? filter = null);
-    Task<TCreate> CreateAsync(TCreate entity);
-    Task<TUpdate> UpdateAsync(TUpdate entity);
-    Task<bool> DeleteAsync(TGet entity);
+    Task<TCreate> CreateAsync(TCreate entityDto);
+    Task<TUpdate> UpdateAsync(TUpdate entityDto);
+    Task<bool> DeleteAsync(TGet entityDto);
 }
