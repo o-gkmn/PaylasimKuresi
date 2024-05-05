0e35c8f9d889c6330ee848ff7e1a235458bf31c1
o_gkmn <ozgur.gokmen735@gmail.com>
2024-05-05 19:45:54 +0300

    Implemented: Post Flow functionality.

--- PaylasimKuresi/ViewComponents/Home/_PostFlowViewComponent.cs
+++ PaylasimKuresi/ViewComponents/Home/_PostFlowViewComponent.cs
@@ -1,11 +1,21 @@
+using Business.PaylasimKuresi.Interfaces.PostServices;
+using DataAccess.Interfaces.PostRepositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PaylasimKuresi.ViewComponents.Home;
 
 public class _PostFlowViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IPostService _postService;
+
+    public _PostFlowViewComponent(IPostService postService)
     {
-        return View();
+        _postService = postService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var postDtoList = await _postService.GetListAsync();
+        return View(postDtoList);
     }
 }
