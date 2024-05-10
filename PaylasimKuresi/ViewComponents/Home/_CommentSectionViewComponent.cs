2c4802c715fbec70d47698913a964accfd6f6509
o_gkmn <ozgur.gokmen735@gmail.com>
2024-05-10 14:09:15 +0300

    Comment feature added

--- PaylasimKuresi/ViewComponents/Home/_CommentSectionViewComponent.cs
+++ PaylasimKuresi/ViewComponents/Home/_CommentSectionViewComponent.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using Models.DTOs.PostDTOs;
 
 namespace PaylasimKuresi.ViewComponents.Home;
 
 public class _CommentSectionViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    public IViewComponentResult Invoke(GetPostDto getPostDto)
     {
-        return View();
+        return View(getPostDto);
     }
 }
